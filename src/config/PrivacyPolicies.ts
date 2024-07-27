de767bcc55c43b2c6b4c453db84ffbd013cae4e2
Danny Nicholas <dannynicholas23@gmail.com>
2024-07-27 09:19:23 +0100

    Update privacy policy comments

--- src/config/PrivacyPolicies.ts
+++ src/config/PrivacyPolicies.ts
@@ -2,6 +2,7 @@ import PlutoPanicIcon from "../assets/projects/pluto-panic/icon.png"
 import { ContentType, PageContent } from "../types/Content"
 
 // holds content for each project's privacy policy
+// the id must match the parent project's id
 export const PrivacyPolicies: PageContent[] = [
     {
         id: 'pluto-panic',
